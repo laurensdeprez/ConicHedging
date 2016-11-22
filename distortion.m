4cb9a53cc9cf41dbe2bf00a51a4e4893ce8cba91
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-22 21:25:06 +0100

    add argument parsing to function: distortion

--- distortion.m
+++ distortion.m
@@ -1,11 +1,18 @@
-function p_dist = distortion(u,label,lambda)
-    if (nargin<3)
-        lambda = 0.25;
-    end 
-    if (nargin<2)
-        label = 'MinMaxVar';
-    end
-    %call appropriate distortion function ("NOT" is just for debugging)
+function p_dist = distortion(u,varargin)
+    p = inputParser;
+    defaultLabel = 'MinMaxVar';
+    validLabel = {'MinVar','MaxVar','MinMaxVar','MaxMinVar','Wang'};
+    checkLabel = @(x) any(validatestring(x,validLabel));
+    defaultLambda = 0.25;
+    addRequired(p,'u');
+    addOptional(p,'label',defaultLabel,checkLabel);
+    addOptional(p,'lambda',defaultLambda);
+    parse(p,u,varargin{:});
+    
+    u = p.Results.u;
+    label = p.Results.label;
+    lambda = p.Results.lambda;
+    % call a distortion function
     switch label 
         case 'MinVar'
             p_dist = MinVar(u,lambda);
@@ -17,10 +24,6 @@ function p_dist = distortion(u,label,lambda)
             p_dist = MaxVar(u,lambda);
         case 'Wang'
             p_dist = WangTrans(u,lambda);
-        case 'NOT' 
-            p_dist = u;
-        otherwise
-            error('label should be one of the following: "MinVar","MaxVar","MinMaxVar","MaxMinVar" or "Wang"')
     end 
 end
 
