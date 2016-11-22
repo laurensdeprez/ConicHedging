d88df340098000b0aa80dc068f4437fe82c2d89d
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-22 22:50:19 +0100

    add argument parsing to payout functions

--- payoff.m
+++ payoff.m
@@ -1,10 +1,18 @@
 function f = payoff(S_T,K,type)
+    p = inputParser();
+    validType = {'call','put'};
+    checkType = @(x) any(validatestring(x,validType));
+    addRequired(p,'S_T',@ispositive);
+    addRequired(p,'K',@ispositive);
+    addRequired(p,'type',checkType);
+    parse(p,S_T,K,type);
+    S_T = p.Results.S_T;
+    K = p.Results.K;
+    type = p.Results.type;
     switch type
         case 'call'
             f = payoff_call(S_T,K);        
         case 'put'
             f = payoff_put(S_T,K);         
-        otherwise
-            error('option_type should be "put" or "call"')
     end
 end
--- payoff_put.m
+++ payoff_put.m
@@ -1,4 +1,10 @@
 function f = payoff_put(S_T,K)
+    p = inputParser();
+    addRequired(p,'S_T',@ispositive);
+    addRequired(p,'K',@ispositive);
+    parse(p,S_T,K);
+    S_T = p.Results.S_T;
+    K = p.Results.K;
     f = max(0,K-S_T);
 end
 
