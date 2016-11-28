1c83061e3ad728990fbec28fbb75192241636b9b
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-28 16:54:43 +0100

    add ask hedging in tri tree

--- conic_dynamic_hedging.m
+++ conic_dynamic_hedging.m
@@ -1,7 +1,7 @@
-close all;
 %% Dynamic Hedging
+close all;
+
 %% Trinomial Tree
-% bid delta hedging
 T = 1/12;           % maturity
 dt = T/2;           % timestep
 S_0 = 100;          % init. stock price
@@ -13,23 +13,49 @@ dist = 'MinMaxVar'; % distortion function
 lambda = 0.125;     % distortion parameter
 % states
 [u,m,d] = states_tri_tree(r,s,dt);
+% Bid hedging
 % intermediate state (hedged)
-[bid_u,~,~,~] = bid_tri_tree(u*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
-[bid_m,~,~,~] = bid_tri_tree(m*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
-[bid_d,~,~,~] = bid_tri_tree(d*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
+[bid_u,~,delta_u,~] = bid_tri_tree(u*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
+[bid_m,~,delta_m,~] = bid_tri_tree(m*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
+[bid_d,~,delta_d,~] = bid_tri_tree(d*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
 disp([bid_u,bid_m,bid_d])
+disp([delta_u,delta_m,delta_d])
 % final state (hedged)
-old_bid = [bid_u,bid_m,bid_d];
-[bid,~,~,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_bid);
-disp(bid)
+old_ask = [bid_u,bid_m,bid_d];
+[ask,~,delta,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_ask);
+disp(ask)
+disp(delta)
 % intermediate state (unhedged)
 [bid_u,~,~,~] = bid_tri_tree(u*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
 [bid_m,~,~,~] = bid_tri_tree(m*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
 [bid_d,~,~,~] = bid_tri_tree(d*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
 disp([bid_u,bid_m,bid_d])
 % final stated (unhedged)
-old_bid = [bid_u,bid_m,bid_d];
-[bid,~,~,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_bid,'hedged',false);
-disp(bid)
+old_ask = [bid_u,bid_m,bid_d];
+[ask,~,~,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_ask,'hedged',false);
+disp(ask)
+
+% Ask hedging
+% intermediate state (hedged)
+[ask_u,~,delta_u,~] = ask_tri_tree(u*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
+[ask_m,~,delta_m,~] = ask_tri_tree(m*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
+[ask_d,~,delta_d,~] = ask_tri_tree(d*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
+disp([ask_u,ask_m,ask_d])
+disp([delta_u,delta_m,delta_d])
+% final state (hedged)
+old_ask = [ask_u,ask_m,ask_d];
+[ask,~,delta,~] = ask_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_ask',old_ask);
+disp(ask)
+disp(delta)
+% intermediate state (unhedged)
+[ask_u,~,~,~] = ask_tri_tree(u*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
+[ask_m,~,~,~] = ask_tri_tree(m*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
+[ask_d,~,~,~] = ask_tri_tree(d*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
+disp([ask_u,ask_m,ask_d])
+% final stated (unhedged)
+old_ask = [ask_u,ask_m,ask_d];
+[ask,~,~,~] = ask_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_ask',old_ask,'hedged',false);
+disp(ask)
+
 %% Multinomial Tree 
 
