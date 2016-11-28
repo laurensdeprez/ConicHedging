d16700c0d5be558a9e8282d567b7bc83cdeccfe9
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-28 12:26:32 +0100

    update characteristic functions to handle vector input

--- char_function_VG.m
+++ char_function_VG.m
@@ -1,7 +1,4 @@
 function phi_VG = char_function_VG(u,C_VG,G_VG,M_VG)
-    phi_VG = zeros(length(u),1);
-    for ii=1:length(u)
-        phi_VG(ii) = (G_VG*M_VG./(G_VG*M_VG+(M_VG-G_VG)*1i*u(ii)+u(ii).^2)).^C_VG;
-    end
+      phi_VG = (G_VG*M_VG./(G_VG*M_VG+(M_VG-G_VG)*1i*u+u.^2)).^C_VG;
 end
 
--- char_function_multinomial.m
+++ char_function_multinomial.m
@@ -1,9 +1,6 @@
 function phi = char_function_multinomial(M,u,delta,p,p_j,q_j,N)
     x = -(1:M)*delta;
     y = (1:M)*delta;
-    phi = zeros(length(u),1);
-    for ii=1:length(u)
-        phi(ii) = (p + (1-p)*sum(p_j.*exp(1i*u(ii)*x)+q_j.*exp(1i*u(ii)*y)))^N;
-    end
+    phi = (p + (1-p)*(p_j*exp(1i*transpose(x)*u)+q_j*exp(1i*transpose(y)*u))).^N;
 end
 
