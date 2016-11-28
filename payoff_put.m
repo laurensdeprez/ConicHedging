75ef1dbd41cc306e14eb87f6a42197ca0226b4dc
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-28 21:28:57 +0100

    change parsing in payout functions

--- payoff_put.m
+++ payoff_put.m
@@ -1,7 +1,7 @@
 function f = payoff_put(S_T,K)
     p = inputParser();
-    addRequired(p,'S_T',@ispositive);
-    addRequired(p,'K',@ispositive);
+    addRequired(p,'S_T',@(x)validateattributes(x,{'numeric'},{'numel',1,'positive'}));
+    addRequired(p,'K',@(x)validateattributes(x,{'numeric'},{'numel',1,'positive'}));
     parse(p,S_T,K);
     S_T = p.Results.S_T;
     K = p.Results.K;
