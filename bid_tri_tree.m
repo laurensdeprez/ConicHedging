a039c1e3374dd67a95692b325353820f0206076b
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-28 16:53:21 +0100

    allow for dynamic hedging in tri tree

--- bid_tri_tree.m
+++ bid_tri_tree.m
@@ -10,6 +10,14 @@ defaultDelta_range = [-2,2];
 addOptional(p,'delta_range',defaultDelta_range,@(x)validateattributes(x,{'numeric'},{'numel',2,'increasing'}));
 defaultDelta_precision = 0.01;
 addOptional(p,'delta_precision',defaultDelta_precision,@ispositive);
+defaultDist = 'MinMaxVar';
+addOptional(p,'dist',defaultDist);
+defaultLambda = 0.01;
+addOptional(p,'lambda',defaultLambda);
+defaultOld_bid = [0,0,0];
+addOptional(p,'old_bid',defaultOld_bid,@(x)validateattributes(x,{'numeric'},{'numel',3}))
+defaultHedged = true;
+addOptional(p, 'hedged',defaultHedged, @(x)validateattributes(x,{'logical'},{'numel',1}))
 parse(p,S_0,s,r,T,K,option,varargin{:});
 S_0 = p.Results.S_0;
 s = p.Results.s;
@@ -19,22 +27,35 @@ K = p.Results.K;
 option = p.Results.option;
 delta_range = p.Results.delta_range;
 delta_precision = p.Results.delta_precision;
+dist = p.Results.dist;
+lambda = p.Results.lambda;
+old_bid = p.Results.old_bid;
+hedged = p.Results.hedged;
 %states
-u = exp((r-s^2/2)*T+s*sqrt(3*T)); % up state
-m = exp((r-s^2/2)*T);             % middle state
-d = exp((r-s^2/2)*T-s*sqrt(3*T)); % down state
+[u,m,d] = states_tri_tree(r,s,T);
 %payouts
-f_u = payoff(u*S_0,K,option);   % option payout up
-f_m = payoff(m*S_0,K,option);   % option payout middle
-f_d = payoff(d*S_0,K,option);   % option payout down
+if (sum(old_bid)==0)
+    f_u = payoff(u*S_0,K,option);   % option payout up
+    f_m = payoff(m*S_0,K,option);   % option payout middle
+    f_d = payoff(d*S_0,K,option);   % option payout down
+else
+    f_u = old_bid(1);
+    f_m = old_bid(2);
+    f_d = old_bid(3);
+end
 %jump probabilities
 p_u = 1/6;                        % jump up 
 p_m = 2/3;                        % jump middle
 p_d = 1/6;                        % jump down
-%optimize    
-n = (delta_range(2)-delta_range(1))/delta_precision;
-deltas = linspace(delta_range(1),delta_range(2),n); 
-bids = zeros(1,n);
+%optimize  
+if hedged
+    n = (delta_range(2)-delta_range(1))/delta_precision;
+    deltas = linspace(delta_range(1),delta_range(2),n); 
+    bids = zeros(1,n);
+else
+    n = 1;
+    deltas = 0;
+end 
 for i=1:n
     pi_u = f_u + deltas(i)*(u - exp(r*T))*S_0;
     pi_m = f_m + deltas(i)*(m - exp(r*T))*S_0;
@@ -45,33 +66,33 @@ for i=1:n
     check = strcat(check(1),check(2),check(3));
     switch check
         case '111'
-            p_dd = distortion(p_d);
-            p_md = distortion(p_m+p_d)-distortion(p_d);
-            p_ud = 1-distortion(p_m+p_d);
+            p_dd = distortion(p_d,dist,lambda);
+            p_md = distortion(p_m+p_d,dist,lambda)-distortion(p_d,dist,lambda);
+            p_ud = 1-distortion(p_m+p_d,dist,lambda);
         case '110'
-            p_md = distortion(p_m);
-            p_dd = distortion(p_m+p_d)-distortion(p_m);
-            p_ud = 1-distortion(p_m+p_d);
+            p_md = distortion(p_m,dist,lambda);
+            p_dd = distortion(p_m+p_d,dist,lambda)-distortion(p_m,dist,lambda);
+            p_ud = 1-distortion(p_m+p_d,dist,lambda);
         case '101'
             error('impossible due to transitivity in bid_tri_tree')
         case '011'
-            p_dd = distortion(p_d);
-            p_ud = distortion(p_u+p_d)-distortion(p_d);
-            p_md = 1-distortion(p_u+p_d);
+            p_dd = distortion(p_d,dist,lambda);
+            p_ud = distortion(p_u+p_d,dist,lambda)-distortion(p_d,dist,lambda);
+            p_md = 1-distortion(p_u+p_d,dist,lambda);
         case '100'
-            p_md = distortion(p_m);
-            p_ud = distortion(p_u+p_m)-distortion(p_m);
-            p_dd = 1-distortion(p_u+p_m);
+            p_md = distortion(p_m,dist,lambda);
+            p_ud = distortion(p_u+p_m,dist,lambda)-distortion(p_m,dist,lambda);
+            p_dd = 1-distortion(p_u+p_m,dist,lambda);
         case '010'
             error('impossible due to transitivity in bid_tri_tree')
         case '001'
-            p_ud = distortion(p_u);
-            p_dd = distortion(p_u+p_d)-distortion(p_u);
-            p_md = 1-distortion(p_u+p_d);
+            p_ud = distortion(p_u,dist,lambda);
+            p_dd = distortion(p_u+p_d,dist,lambda)-distortion(p_u,dist,lambda);
+            p_md = 1-distortion(p_u+p_d,dist,lambda);
         case '000'
-            p_ud = distortion(p_u) ;
-            p_md = distortion(p_u+p_m)-distortion(p_u);
-            p_dd = 1-distortion(p_u+p_m);
+            p_ud = distortion(p_u,dist,lambda);
+            p_md = distortion(p_u+p_m,dist,lambda)-distortion(p_u,dist,lambda);
+            p_dd = 1-distortion(p_u+p_m,dist,lambda);
         otherwise
             error('something went wrong in bid_tri_tree')
     end 
