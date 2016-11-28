c4c014d6172a65e55a240ce9637c376494b96a6e
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-28 21:33:20 +0100

    update characteristic function for multinom tree

--- char_function_multinomial.m
+++ char_function_multinomial.m
@@ -1,4 +1,7 @@
-function phi = char_function_multinomial(M,u,delta,p,p_j,q_j,N)
+function phi = char_function_multinomial(M,u,delta,p,N,C_VG,G_VG,M_VG)
+    ps = cond_prob_multinomial(M,delta,C_VG,G_VG,M_VG);
+    p_j = ps(1,:);
+    q_j = ps(2,:);
     x = -(1:M)*delta;
     y = (1:M)*delta;
     phi = (p + (1-p)*(p_j*exp(1i*transpose(x)*u)+q_j*exp(1i*transpose(y)*u))).^N;
