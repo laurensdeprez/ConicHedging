113536c5b1e0ef46f979aa79dd23d641f8621f55
Laurens Deprez <laurens-deprez@hotmail.com>
2016-11-28 21:34:29 +0100

    try dynamic hedging in multinomial

--- conic_dynamic_hedging.m
+++ conic_dynamic_hedging.m
@@ -1,7 +1,10 @@
 %% Dynamic Hedging
 close all;
+tri = false;
+multi = true;
 
 %% Trinomial Tree
+if tri 
 T = 1/12;           % maturity
 dt = T/2;           % timestep
 S_0 = 100;          % init. stock price
@@ -13,6 +16,7 @@ dist = 'MinMaxVar'; % distortion function
 lambda = 0.125;     % distortion parameter
 % states
 [u,m,d] = states_tri_tree(r,s,dt);
+
 % Bid hedging
 % intermediate state (hedged)
 [bid_u,~,delta_u,~] = bid_tri_tree(u*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda);
@@ -21,9 +25,9 @@ lambda = 0.125;     % distortion parameter
 disp([bid_u,bid_m,bid_d])
 disp([delta_u,delta_m,delta_d])
 % final state (hedged)
-old_ask = [bid_u,bid_m,bid_d];
-[ask,~,delta,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_ask);
-disp(ask)
+old_bid = [bid_u,bid_m,bid_d];
+[bid,~,delta,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_bid);
+disp(bid)
 disp(delta)
 % intermediate state (unhedged)
 [bid_u,~,~,~] = bid_tri_tree(u*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
@@ -31,9 +35,9 @@ disp(delta)
 [bid_d,~,~,~] = bid_tri_tree(d*S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'hedged',false);
 disp([bid_u,bid_m,bid_d])
 % final stated (unhedged)
-old_ask = [bid_u,bid_m,bid_d];
-[ask,~,~,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_ask,'hedged',false);
-disp(ask)
+old_bid = [bid_u,bid_m,bid_d];
+[bid,~,~,~] = bid_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_bid',old_bid,'hedged',false);
+disp(bid)
 
 % Ask hedging
 % intermediate state (hedged)
@@ -55,7 +59,32 @@ disp([ask_u,ask_m,ask_d])
 % final stated (unhedged)
 old_ask = [ask_u,ask_m,ask_d];
 [ask,~,~,~] = ask_tri_tree(S_0,s,r,dt,K,option,'dist',dist,'lambda',lambda,'old_ask',old_ask,'hedged',false);
-disp(ask)
-
+disp(['ask price ',num2str(ask)])
+end
 %% Multinomial Tree 
+T = 1;                      % maturity (in years)
+dt = 1/50;                  % timestep (in years)
+
+S_0 = 100;                  % init stock value
+r = 0.02;                   % risk-free interest
+K = [100,120];              % call spread strikes
+option = 'callspread';      % option type
+
+sigma = 0.2;                % VG process parameters
+nu = 0.75;
+theta = -0.3;
+C_VG = 1/v;
+G_VG = 1/(sqrt(th^2*v^2/4+s^2*v/2)-th*v/2);
+M_VG = 1/(sqrt(th^2*v^2/4+s^2*v/2)+th*v/2);
+
+dist = 'MinMaxVar';         % distortion type
+lambda = 0.015;             % distortion parameter
+
+M = 10;                     % 2M+1-nomial approximation
+p = 0.9407;                 % jump zero probability (after optim)
+delta = 0.0740;             % jump size (after optim)
+
+ps = cond_prob_multinomial(M,delta,C_VG,G_VG,M_VG);
+p_j = ps(1,:);              % conditional prob 
+q_j = ps(2,:);
 
