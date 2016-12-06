2bdc6cd9a53104553fb6e4be4b839209a6be4f9b
Laurens Deprez <laurens-deprez@hotmail.com>
2016-12-06 11:35:56 +0100

    call VG_param

--- multinomial_approx_VG.m
+++ multinomial_approx_VG.m
@@ -7,9 +7,7 @@ p = 0.3;        % probability for level 0
 s = 0.2;        
 v = 0.75;
 th = -0.3;
-C_VG = 1/v;
-G_VG = 1/(sqrt(th^2*v^2/4+s^2*v/2)-th*v/2);
-M_VG = 1/(sqrt(th^2*v^2/4+s^2*v/2)+th*v/2);
+[C_VG,G_VG,M_VG] = VG_param(s,v,th);
 
 U = linspace(-20,20,1000);
 phis = char_function_multinomial(M,U,delta,p,N,C_VG,G_VG,M_VG);
@@ -32,14 +30,14 @@ title(['p = ',num2str(p),' en delta = ',num2str(delta)])
 
 
 %% optimize
-x0 = [0.0740,0.9407];%[delta,p];%
+if false
+x0 = [0.07,0.9];
 x = fit_multinomial_VG(M,N,C_VG,G_VG,M_VG,x0);
 delta = x(1);
 p = x(2);
 disp(x)
 
-
-phis = char_function_multinomial(M,u,delta,p,N,C_VG,G_VG,M_VG);
+phis = char_function_multinomial(M,U,delta,p,N,C_VG,G_VG,M_VG);
 
 figure()
 plot(U,real(phis))
@@ -55,3 +53,4 @@ plot(U,imag(phis_VG))
 axis([-20,20,-0.5,0.5])
 legend('multinomial','VG')
 title(['p = ',num2str(p),' en delta = ',num2str(delta)])
+end
