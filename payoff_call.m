139a9b7eecc21eb66eb7a79edce548a2a6b113af
Laurens Deprez <laurens-deprez@hotmail.com>
2016-12-06 11:36:42 +0100

    handle vector input

--- payoff_call.m
+++ payoff_call.m
@@ -1,6 +1,6 @@
 function f = payoff_call(S_T,K)
     p = inputParser();
-    addRequired(p,'S_T',@(x)validateattributes(x,{'numeric'},{'numel',1,'positive'}));
+    addRequired(p,'S_T',@(x)validateattributes(x,{'numeric'},{'positive'}));
     addRequired(p,'K',@(x)validateattributes(x,{'numeric'},{'numel',1,'positive'}));
     parse(p,S_T,K);
     S_T = p.Results.S_T;
--- payoff_callspread.m
+++ payoff_callspread.m
@@ -1,10 +1,10 @@
 function f = payoff_callspread(S_T,K)
     p = inputParser();
-    addRequired(p,'S_T',@(x)validateattributes(x,{'numeric'},{'numel',1,'increasing','positive'}));
+    addRequired(p,'S_T',@ispositive);
     addRequired(p,'K',@(x)validateattributes(x,{'numeric'},{'numel',2,'increasing','positive'}));
     parse(p,S_T,K);
     S_T = p.Results.S_T;
     K = p.Results.K;
-    f = max(0,S_T-K(1))-max(0,S_T-120);
+    f = max(0,S_T-K(1))-max(0,S_T-K(2));
 end
 
