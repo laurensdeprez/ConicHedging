d93cc151b443063a04ba5e83b571f0e627ba19fb
Laurens Deprez <laurens-deprez@hotmail.com>
2017-02-11 14:53:56 +0100

    adding risk neutral pricing and upgrade plots

--- conic_delta_hedging_B_S.m
+++ conic_delta_hedging_B_S.m
@@ -1,6 +1,5 @@
 close all;
 %% conic delta hedging under B-S
-
 S_0 = 100;                       % init. stock price
 q = 0;                           % trend 
 s = 0.2;                         % volatility
@@ -9,48 +8,64 @@ T = 1/12;                        % maturity
 K = S_0;                         % strike ATM
 N = 10000;                       % # monte carlo simulations (WARNING: bigger=slower)
 dist_type = 'MinMaxVar';         % distortion function
-lambda = 0.25;                   % parameter for distortion               
+lambda = 0.10;                   % parameter for distortion               
 delta_range = [-2,2];            % [delta_min, delta_max]
 delta_precision = 0.01;          % step between deltas
 option = 'call';                 % type of option considered
-different_strikes = 1;
-%% bid
+different_strikes = 0;
 
-[bid,bids,delta,deltas] = bid_B_S(S_0,q,s,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision);
+%% risk neutral
+% call option
+price = risk_neutral_EC_B_S(S_0,s,q,r,T,K);
+
+%% bid
+[bid,bids,~,deltas] = bid_B_S(S_0,q,s,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision);
+[u_bid,~,~,~] = bid_B_S(S_0,q,s,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision,'hedged',false);
 figure()
-plot(deltas, bids)
-hold on
-scatter(delta,bid,'b')
-xlabel('delta')
-ylabel('bid portfolio')
+plot(deltas, bids,'LineWidth',2)
+hold on 
+plot(deltas, u_bid*ones(1,length(deltas)) ,'b--','LineWidth',2)
+xlabel('\Delta','FontSize',15)
+ylabel('bid portfolio','FontSize',15)
+leg = legend('\Delta hedged','unhedged');
+set(gca,'fontsize',12)
+set(leg,'fontsize',12)
 
 %% ask 
-[ask,asks,delta,deltas] = ask_B_S(S_0,q,s,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision);
+[ask,asks,~,deltas] = ask_B_S(S_0,q,s,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision);
+[u_ask,~,~,~] = ask_B_S(S_0,q,s,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision,'hedged',false);
 figure()
-plot(deltas, asks)
-hold on
-scatter(delta,ask,'b')
-xlabel('delta')
-ylabel('ask portfolio')
+plot(deltas, asks,'LineWidth',2)
+hold on 
+plot(deltas, u_ask*ones(1,length(deltas)) ,'b--','LineWidth',2)
+xlabel('\Delta','FontSize',15)
+ylabel('ask portfolio','FontSize',15)
+leg = legend('\Delta hedged','unhedged');
+set(gca,'fontsize',12)
+set(leg,'fontsize',12)
 
 %% capital 
 figure();
 capital = asks-bids;
+u_cap = u_ask-u_bid;
 [M,I] = min(capital);
-plot(deltas_bid,capital)
-hold on
-scatter(deltas(I),M,'b')
-xlabel('delta')
-ylabel('capital portfolio')
+plot(deltas,capital,'LineWidth',2)
+hold on 
+plot(deltas, u_cap*ones(1,length(deltas)) ,'b--','LineWidth',2)
+xlabel('\Delta','FontSize',15)
+ylabel('capital portfolio','FontSize',15)
+leg = legend('\Delta hedged','unhedged');
+set(gca,'fontsize',12)
+set(leg,'fontsize',12)
 
 if different_strikes
 %% different strikes
-K = linspace(85,115,60);            % strikes
+K = linspace(85,115,240);            % strikes
 delta_bid = zeros(length(K),1);     % delta bid
 delta_ask = zeros(length(K),1);     % delta ask
 delta_capital = zeros(length(K),1); % delta capital
 for j=1:length(K)
-    [bid,bids,delta_bid(j),deltas] = bid_B_S(S_0,q,s,r,T,N,K(j),option,dist_type,lambda,delta_range,delta_precision);
+    [bid,bids,delta_bid(j),~] = bid_B_S(S_0,q,s,r,T,N,K(j),option,dist_type,lambda,delta_range,delta_precision);
     [ask,asks,delta_ask(j),deltas] = ask_B_S(S_0,q,s,r,T,N,K(j),option,dist_type,lambda,delta_range,delta_precision);
     [M,I]= min(asks-bids);
     delta_capital(j) = deltas(I);
