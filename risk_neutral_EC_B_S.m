4e29422da41d745fdb15714d9c858ed483e89803
Laurens Deprez <laurens-deprez@hotmail.com>
2017-02-11 18:12:32 +0100

    function to risk neutral price EC in BS

---
+++ risk_neutral_EC_B_S.m
@@ -0,0 +1,23 @@
+function price = risk_neutral_EC_B_S(S_0,s,q,r,T,K)
+p = inputParser;
+addRequired(p,'S_0');
+addRequired(p,'s',@ispositive);
+addRequired(p,'q',@ispositive);
+addRequired(p,'r',@ispositive);
+addRequired(p,'T',@ispositive);
+addRequired(p,'K');
+parse(p,S_0,s,q,r,T,K);
+S_0 = p.Results.S_0;
+s = p.Results.s;
+q = p.Results.q;
+r = p.Results.r;
+T = p.Results.T;
+K = p.Results.K;
+
+d1 = (log(S_0/K)+(r-q+s^2/2)*T)/(s*sqrt(T));
+d2 = d1 - s*sqrt(T);
+
+%price
+price = exp(-q*T)*S_0*normcdf(d1) - exp(-r*T)*K*normcdf(d2);
+end
+
