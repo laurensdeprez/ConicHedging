7a675df83276cef8875c47346b18e1353ae989d2
Laurens Deprez <laurens-deprez@hotmail.com>
2017-02-11 11:33:21 +0100

    update output of scripts

--- conic_delta_hedging_bin_tree.m
+++ conic_delta_hedging_bin_tree.m
@@ -1,13 +1,14 @@
 %% conic delta hedging in binomial tree
 S_0 = 100;                      % init. stock price
-u = 1.2;                        % up state
-d = 0.8;                        % down state
+s = 0.2;                        % volatility
 r = 0.01;                       % interest
 T = 1;                          % maturity
 K = S_0;                        % strike ATM
 delta_range = [-2,2];           % [delta_min, delta_max]
 delta_precision = 0.001;        % step between deltas
 type = 'call';                  % option type
+%% up and down state
+[u,d] = states_bin_tree(s,T);
 
 %% risk neutral
 f_u = payoff(u*S_0,K,type);         % option payout up
@@ -19,24 +20,27 @@ delta_tree = (f_u-f_d)/(S_0*(u-d));
 [bid,bids,delta,deltas] = bid_bin_tree(S_0,u,d,r,T,K,type,delta_range,delta_precision);
 
 figure()
-plot(deltas, bids)
+plot(deltas, bids,'LineWidth',2)
 hold on
-scatter(delta,bid,'b')
-scatter(-delta_tree,bid,100,'r')
-xlabel('delta')
-ylabel('bid portfolio')
-legend('\Delta line','\Delta_{opt}','-\Delta_{tree}')
+scatter(delta,bid,40,'b','filled')
+scatter(-delta_tree,bid,80,'r','LineWidth',2)
+xlabel('\Delta','FontSize',15)
+ylabel('bid portfolio','FontSize',15)
+leg = legend('\Delta line','\Delta_{opt}','-\Delta_{tree}');
+set(leg,'FontSize',12)
+set(gca,'fontsize',12)
 
 %% ask
 [ask,asks,delta,deltas] = ask_bin_tree(S_0,u,d,r,T,K,type,delta_range,delta_precision);
 
 figure()
-plot(deltas, asks)
+plot(deltas, asks,'LineWidth',2)
 hold on
-scatter(delta,ask,'b')
-scatter(-delta_tree,ask,100,'r')
-xlabel('delta')
-ylabel('ask portfolio')
-legend('\Delta line','\Delta_{opt}','-\Delta_{tree}')
-
+scatter(delta,ask,40,'b','filled')
+scatter(-delta_tree,ask,80,'r','LineWidth',2)
+xlabel('\Delta','FontSize',15)
+ylabel('ask portfolio','FontSize',15)
+leg = legend('\Delta line','\Delta_{opt}','-\Delta_{tree}');
+set(leg,'FontSize',12)
+set(gca,'fontsize',12)
 %% capital
--- conic_delta_hedging_tri_tree.m
+++ conic_delta_hedging_tri_tree.m
@@ -1,5 +1,4 @@
 %% conic delta hedging in trinomial tree
-
 S_0 = 100;                       % init. stock price
 s = 0.2;                         % volatility
 r = 0.01;                        % interest
@@ -8,24 +7,29 @@ K = S_0;                         % strike ATM
 delta_range = [-2,2];            % [delta_min, delta_max]
 delta_precision = 0.001;         % step between deltas
 option_type = 'call';            % option type
-%% bid
 
-[bid,bids,delta,deltas] = bid_tri_tree(S_0,s,r,T,K,option_type,delta_range,delta_precision);
+%% risk neutral
+price = risk_neutral_tri(S_0,s,r,T,K,option_type);
+
+%% bid
+[~,bids,delta_bid,deltas] = bid_tri_tree(S_0,s,r,T,K,option_type,delta_range,delta_precision);
 
 figure()
-plot(deltas, bids)
-hold on
-scatter(delta,bid,'b')
-xlabel('delta')
-ylabel('bid portfolio')
+plot(deltas, bids,'LineWidth',2)
+xlabel('\Delta','FontSize',15)
+ylabel('bid portfolio','FontSize',15)
+set(gca,'fontsize',12)
 
 %% ask
-
-[ask,asks,delta,deltas] = ask_tri_tree(S_0,s,r,T,K,option_type,delta_range,delta_precision);
+[~,asks,delta_ask,deltas] = ask_tri_tree(S_0,s,r,T,K,option_type,delta_range,delta_precision);
 
 figure()
-plot(deltas, asks)
-hold on
-scatter(delta,ask,'b')
-xlabel('delta')
-ylabel('ask portfolio')
+plot(deltas, asks,'LineWidth',2)
+xlabel('\Delta','FontSize',15)
+ylabel('ask portfolio','FontSize',15)
+set(gca,'fontsize',12)
+
+%% unhedged bid and ask
+[bid,~,~,~] = bid_tri_tree(S_0,s,r,T,K,option_type,delta_range,delta_precision,'hedged',false);
+
+[ask,~,~,~] = ask_tri_tree(S_0,s,r,T,K,option_type,delta_range,delta_precision,'hedged',false);
