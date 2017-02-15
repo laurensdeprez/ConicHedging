a6780af7036dcc0225e2b4530d83b47413a04c1b
Laurens Deprez <laurens-deprez@hotmail.com>
2017-02-15 17:45:29 +0100

    add function for B-S stock model

---
+++ B_S.m
@@ -0,0 +1,19 @@
+function S_T = B_S(S_0,q,s,r,T,N)
+    p = inputParser;
+    addRequired(p,'S_0',@ispositive);
+    addRequired(p,'q',@isnumeric);
+    addRequired(p,'s',@ispositive);
+    addRequired(p,'r',@ispositive);
+    addRequired(p,'T',@ispositive);
+    addRequired(p,'N',@ispositive);
+    parse(p,S_0,q,s,r,T,N);
+    S_0 = p.Results.S_0;
+    s = p.Results.s;
+    r = p.Results.r;
+    T = p.Results.T;
+    N = p.Results.N;
+    % Monte Carlo 
+    W = normrnd(0,sqrt(T),[N,1]);
+    S_T = S_0*exp((r-q-s^2/2)*T+s*W);
+end
+
