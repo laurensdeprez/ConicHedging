2b0b0009208949659dcf8350c60b3896c92e7186
Laurens Deprez <laurens-deprez@hotmail.com>
2017-02-15 17:47:09 +0100

    add general bid and ask functions for cont. models

---
+++ bid_cont_model.m
@@ -0,0 +1,51 @@
+function [bid,bids,delta,deltas] = bid_cont_model(S_0,S_T,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision,hedged)
+p = inputParser;
+addRequired(p,'S_0');
+addRequired(p,'S_T');
+addRequired(p,'r');
+addRequired(p,'T');
+addRequired(p,'N');
+addRequired(p,'K');
+addRequired(p,'option');
+addRequired(p,'dist_type')
+addRequired(p,'lambda')
+addRequired(p,'delta_range');
+addRequired(p,'delta_precision');
+addRequired(p, 'hedged')
+parse(p,S_0,S_T,r,T,N,K,option,dist_type,lambda,delta_range,delta_precision,hedged);
+S_0 = p.Results.S_0;
+S_T = p.Results.S_T;
+r = p.Results.r;
+T = p.Results.T;
+N = p.Results.N;
+K = p.Results.K;
+option = p.Results.option;
+dist_type = p.Results.dist_type;
+lambda = p.Results.lambda;
+delta_range = p.Results.delta_range;
+delta_precision = p.Results.delta_precision;
+hedged = p.Results.hedged;
+% payoff
+f = payoff(S_T,K,option);
+% distorted probabilities
+prob_dist = zeros(1,N);
+for i=1:N
+    prob_dist(i) = distortion(i/N,dist_type,lambda)- distortion((i-1)/N,dist_type,lambda);
+end
+% optimize
+if hedged
+    n = (delta_range(2)-delta_range(1))/delta_precision;
+    deltas = linspace(delta_range(1),delta_range(2),n); 
+else
+    n = 1;
+    deltas = 0;
+end 
+bids = zeros(1,n);
+for i=1:n
+    pi = sort(f + deltas(i)*(S_T - exp(r*T)*S_0));
+    bids(i) = exp(-r*T)*prob_dist*pi;
+end
+[bid,I] = max(bids);
+delta = deltas(I);
+end
+
