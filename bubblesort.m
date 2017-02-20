0a8454ad1fae6083ffb7908af39d559a63bfcb51
Laurens Deprez <laurens-deprez@hotmail.com>
2017-02-20 17:37:20 +0100

    add argument parsing and descend sorting

--- bubblesort.m
+++ bubblesort.m
@@ -1,11 +1,28 @@
-function [A,I] = bubblesort(A)
+function [A,I] = bubblesort(A,varargin)
+    p = inputParser;
+    addRequired(p,'A');
+    validType = {'descend','ascend'};
+    defaultType = 'ascend';
+    checkType = @(x) any(validatestring(x,validType));
+    addOptional(p,'type',defaultType,checkType);
+    parse(p,A,varargin{:});
+    A = p.Results.A;
+    type = p.Results.type;
     I = linspace(1,length(A),length(A));
     for ii=1:length(A)
         for k =length(A):-1:(ii+1)
-            if (A(k)<A(k-1))
-                A = swap(A,k,k-1);
-                I = swap(I,k,k-1);
-            end
+            switch type
+                case 'ascend'
+                    if (A(k)<A(k-1))
+                        A = swap(A,k,k-1);
+                        I = swap(I,k,k-1);
+                    end
+                case 'descend'
+                    if (A(k)>A(k-1))
+                        A = swap(A,k,k-1);
+                        I = swap(I,k,k-1);
+                    end
+            end 
         end
     end
 end
