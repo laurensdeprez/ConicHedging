585ad8dabe7c7352dc4d471e98dfa01bb0818a8e
Laurens Deprez <laurens-deprez@hotmail.com>
2017-03-12 15:22:57 +0100

    add delta gamma hedging

--- conic_delta_hedging_bin_tree.m
+++ conic_delta_hedging_bin_tree.m
@@ -1,3 +1,5 @@
+clear;
+close all;
 %% conic delta hedging in binomial tree
 S_0 = 100;                      % init. stock price
 s = 0.2;                        % volatility
@@ -5,7 +7,7 @@ r = 0.01;                       % interest
 T = 1;                          % maturity
 K = S_0;                        % strike ATM
 delta_range = [-2,2];           % [delta_min, delta_max]
-delta_precision = 4000;         % step between deltas
+delta_precision = 400;          % step between deltas
 type = 'call';                  % option type
 %% up and down state
 [u,d] = states_bin_tree(s,T);
@@ -16,8 +18,8 @@ f_d = payoff(d*S_0,K,type);         % option payout down
 
 delta_tree = (f_u-f_d)/(S_0*(u-d));
 
-%% bid
-[bid,bids,delta,deltas] = bid_bin_tree(S_0,u,d,r,T,K,type,delta_range,delta_precision);
+%% bid (delta)
+[bid,bids,delta,deltas] = bid_bin_tree(S_0,s,r,T,K,type,delta_range,delta_precision);
 
 figure()
 plot(deltas, bids,'LineWidth',2)
@@ -30,8 +32,8 @@ leg = legend('\Delta line','\Delta_{opt}','-\Delta_{tree}');
 set(leg,'FontSize',12)
 set(gca,'fontsize',12)
 
-%% ask
-[ask,asks,delta,deltas] = ask_bin_tree(S_0,u,d,r,T,K,type,delta_range,delta_precision);
+%% ask (delta)
+[ask,asks,delta,deltas] = ask_bin_tree(S_0,s,r,T,K,type,delta_range,delta_precision);
 
 figure()
 plot(deltas, asks,'LineWidth',2)
@@ -43,4 +45,25 @@ ylabel('ask portfolio','FontSize',15)
 leg = legend('\Delta line','\Delta_{opt}','-\Delta_{tree}');
 set(leg,'FontSize',12)
 set(gca,'fontsize',12)
-%% capital
+
+%% bid (delta-gamma)
+[bid,bids,delta_b,deltas,gamma_b,gammas] = bid_bin_tree(S_0,s,r,T,K,type,'hedging_type','Delta-Gamma','delta_range',delta_range,'delta_precision',delta_precision);
+
+figure()
+colormap('jet')
+contourf(gammas,flip(deltas),flipud(transpose(bids)),20);
+xlabel('\Gamma','FontSize',15)
+ylabel('\Delta','FontSize',15)
+c = colorbar;
+ylabel(c,'bid price','FontSize',15)
+
+%% ask (delta-gamma)
+[ask,asks,delta_a,deltas,gamma_a,gammas] = ask_bin_tree(S_0,s,r,T,K,type,'hedging_type','Delta-Gamma','delta_range',delta_range,'delta_precision',delta_precision);
+
+figure()
+colormap('jet')
+contourf(gammas,flip(deltas),flipud(transpose(asks)),20);
+xlabel('\Gamma','FontSize',15)
+ylabel('\Delta','FontSize',15)
+c = colorbar;
+ylabel(c,'ask price','FontSize',15)
