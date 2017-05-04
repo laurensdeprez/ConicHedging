a2763a2474b253ac18b3fa71676e2a7f4a5a5eb5
Laurens Deprez <laurens-deprez@hotmail.com>
2017-05-04 14:50:42 +0200

    clean up B-S simulation function

--- B_S.m
+++ B_S.m
@@ -1,12 +1,18 @@
+% This file is part of ConicHedging
+% Copyright (c) 2017 Laurens Deprez and Wim Schoutens
+% License: BSD 3-clause (see file LICENSE)
+
+%% Simulation of N stock prices S_T under Black-Scholes
 function S_T = B_S(S_0,q,s,r,T,N)
     p = inputParser;
     addRequired(p,'S_0',@ispositive);
-    addRequired(p,'q',@isnumeric);
+    addRequired(p,'q',@ispositive);
     addRequired(p,'s',@ispositive);
     addRequired(p,'r',@ispositive);
     addRequired(p,'T',@ispositive);
     addRequired(p,'N',@ispositive);
     parse(p,S_0,q,s,r,T,N);
+    
     S_0 = p.Results.S_0;
     q = p.Results.q;
     s = p.Results.s;
